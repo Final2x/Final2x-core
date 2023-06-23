648ff015c35e6626fbe01a155f05413002003a1e
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-06-23 23:00:53 +0800

    CI build

---
+++ tests/CIsetconfig.py
@@ -0,0 +1,40 @@
+import cv2
+import yaml
+from pathlib import Path
+
+if __name__ == '__main__':
+    projectPATH = Path(__file__).resolve().parent.parent.absolute()
+
+    gpuid: int = 0  # -1 for CPU, > 0 for GPU
+    # use cpu if gpu is not available
+    if not cv2.ocl.haveOpenCL():
+        gpuid = -1
+
+    p_dict = {
+        "gpuid"      : gpuid,
+        "inputpath"  : [
+            "./1/1/4/5/1/4/1/9/1/9/8/1/0.jpg",
+            str(projectPATH / "assets" / "herta.jpg"),
+            str(projectPATH / "assets" / "herta.jpg"),
+            str(projectPATH / "assets" / "final2x-10.png"),
+            str(projectPATH / "assets" / "final2x-20.png"),
+            str(projectPATH / "assets" / "final2x-40.png"),
+            str(projectPATH / "assets" / "final2x-80.png"),
+            str(projectPATH / "assets" / "final2x-160.png"),
+            str(projectPATH / "assets" / "final2x-320.png"),
+            str(projectPATH / "assets" / "final2x-640.png"),
+            str(projectPATH / "assets" / "final2x-1280.png"),
+        ],
+        "model"      : "RealCUGAN-pro",
+        "modelscale" : 2,
+        "modelnoise" : 1,
+        "outputpath" : str(projectPATH / "assets"),
+        "targetscale": 2,
+        "tta"        : True,
+    }
+
+    p_model: str = str(projectPATH / "models")
+    p_yaml = str(projectPATH / "config.yaml")
+
+    with open(p_yaml, 'w', encoding='utf-8') as f:
+        yaml.dump(p_dict, f)
