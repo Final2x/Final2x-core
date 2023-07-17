ae09db3abf7199fa066bc8854ee9f9f6c2876953
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-07-17 09:58:42 +0800

    update __init__.py, add import

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 # 别看了， 啥也没有
+# for pip ver import
+from Final2x_core.src.utils.getConfig import SRCONFIG
+from Final2x_core.src.SRqueue import SR_queue
+from Final2x_core.src.SRFactory.SRFactory import SRFactory
+
+__all__ = ['SRCONFIG', 'SR_queue', 'SRFactory']
