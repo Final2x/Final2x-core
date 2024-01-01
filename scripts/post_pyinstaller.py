a8cc2d4793fde1444d5036554aad5ac4c2a53653
tohru <65994850+Tohrusky@users.noreply.github.com>
2024-01-02 05:38:46 +0800

    dist

--- scripts/post_pyinstaller.py
+++ scripts/post_pyinstaller.py
@@ -1,17 +1,22 @@
 import shutil
+import sys
 from pathlib import Path
 
 projectPATH = Path(__file__).resolve().parent.parent.absolute()
 
 
-def copy_models_and_config() -> None:
+def post_pyinstaller() -> None:
+    print("-" * 50)
     shutil.copytree(projectPATH / "src/Final2x_core/models", projectPATH / "dist/Final2x-core/models")
     shutil.copy(projectPATH / "src/Final2x_core/config.yaml", projectPATH / "dist/Final2x-core/config.yaml")
-
-    print("-" * 50)
     print("Copied models and config to dist folder~")
+
+    if sys.platform == "win32":
+        shutil.copy(projectPATH / "assets/vulkan-1.dll", projectPATH / "dist/Final2x-core/vulkan-1.dll")
+        print("Copied vulkan-1.dll to dist folder~")
+
     print("-" * 50)
 
 
 if __name__ == "__main__":
-    copy_models_and_config()
+    post_pyinstaller()
