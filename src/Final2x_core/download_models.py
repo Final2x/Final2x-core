3fb508005241fc7925bc1635d7b5eff51d37e279
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-06-28 23:39:17 +0800

    fix: folder hash error

--- src/Final2x_core/download_models.py
+++ src/Final2x_core/download_models.py
@@ -22,15 +22,15 @@ else:
 model_dict: Dict[str, List[str]] = {
     "RealCUGAN": [
         "https://github.com/Tohrusky/model-zoo/releases/download/v2.0.0/RealCUGAN-240628.zip",
-        "3392e740cda8c9e5308f33749da7ea76c1dbb972f4959db7745cfadbb8e0079f",
+        "6f9d1f3fd06354647f33414c265b855b5bf5b41dacc80c00ce2fcdd56f9de8a6",
     ],
     "RealESRGAN": [
         "https://github.com/Tohrusky/model-zoo/releases/download/v2.0.0/RealESRGAN-240628.zip",
-        "424ed35216c7045f28a48c04cb9bda96bba5e0abe45419eaf8914005d98c18fb",
+        "7408c4c865847ffb3dd98aa9375edddcc7eadc584239752d3d9e32129a5cf719",
     ],
     "Waifu2x": [
         "https://github.com/Tohrusky/model-zoo/releases/download/v2.0.0/Waifu2x-240628.zip",
-        "9db18b02102ab927fe89dc1689e599198226d585c6d2f80137347312e7ec48da",
+        "edae59c6f30a39d0d798a077d39eb2a62d156076d8f632c0854b2f0ae782d313",
     ],
 }
 
@@ -57,9 +57,9 @@ def hash_directory(directory_path: Path) -> str:
             files_list.append(hash_file(filepath))
 
     # Sort by file path to ensure consistency of the result
-    files_list.sort(key=lambda x: x[0])
+    files_list.sort()
     # Concatenate the paths and hashes of all files
-    concatenated_str = "".join([f"{hash}\n" for hash in files_list])
+    concatenated_str = "".join([f"{hash}" for hash in files_list])
     # Calculate the final hash value
     final_hash_sha256 = hashlib.sha256()
     final_hash_sha256.update(concatenated_str.encode("utf-8"))
